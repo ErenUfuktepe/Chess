8f8eb59221ebcd824f41c438bf55e767a96578f7
Eren Ufuktepe <erenufuktepe@gmail.com>
2023-11-14 14:19:10 -0600

    optimize imports

--- src/main/pieces/King.java
+++ src/main/pieces/King.java
@@ -1,12 +1,7 @@
 package main.pieces;
 
-import main.Position;
-import main.Rule;
 import main.enums.Color;
 import main.enums.PieceType;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class King extends Piece {
 
